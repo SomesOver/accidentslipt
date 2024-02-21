89e7d54d0f83e1f01176db1afe3dbc0fd554db84
SomesOver <160035610+SomesOver@users.noreply.github.com>
2024-02-22 00:18:18 +0800

    Update config.lua

--- lua/accidentslipt/config.lua
+++ lua/accidentslipt/config.lua
@@ -14,7 +14,28 @@ local M = {
 
 function M:merge_options(opts)
 	if type(opts) == "table" and opts ~= {} then
-		self = vim.tbl_deep_extend("force", self, opts)
+		M = vim.tbl_deep_extend("force", M, opts)
+	end
+end
+
+function M.highlight()
+	local opts = M.highlight
+
+	function check_version(major, minor, patch)
+		return major >= vim.version()["major"] and minor >= vim.version()["minor"] and patch >= vim.version()["patch"]
+	end
+
+	if check_version(0, 9, 0) then
+		-- `nvim_get_hl` is added in 0.9.0
+		if vim.tbl_isempty(vim.api.nvim_get_hl(0, { name = "NvimSeparator" })) then
+			vim.api.nvim_set_hl(0, "NvimSeparator", opts)
+		end
+	else
+		-- if name is not existed, `nvim_get_hl_by_name` return an error
+		local ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NvimSeparator", false)
+		if not ok then
+			vim.api.nvim_set_hl(0, "NvimSeparator", opts)
+		end
 	end
 end
 
